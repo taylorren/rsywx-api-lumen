6b21963ccc6733125b22581a198adc8c98f9ecd1
Taylor Ren <taylor.ren@gmail.com>
2023-03-07 17:07:24 +0800

    Use database to get latest blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Laravel\Lumen\Http\Request;
 use Illuminate\Support\Facades\DB;
-
 use Unirest;
+
 /**
  * This is the Controller for Book related operations
  */
@@ -24,29 +24,18 @@ class BlogController extends BaseController {
      * @return JSON
      */
     public function latest($count = 1) {
-        $uri = "https://blog.rsywx.net/wp-json/wp/v2/posts?per_page=$count";
-
-        $out = Unirest\Request::get($uri)->body;
+        $sql = "select wp.post_date pd, wp.post_excerpt excerpt, wpm2.meta_value media, wp.guid link, wp.post_title title
+from wp_postmeta wpm1, wp_postmeta wpm2, wp_posts wp
+where wpm1.meta_value=wpm2.post_id
+and wpm1.meta_key like '%thumb%'
+and wpm2.meta_key like '%attached%'
+and wp.ID=wpm1.post_id
+order by wp.post_date desc
+limit 0," . $count;
+        $res=DB::connection('blog')->select($sql);
         
-        $res = [];
-        foreach ($out as $o) {
-            
-            $title = $o->title->rendered;
-            $excerpt = $o->excerpt->rendered;
-            $link = $o->link;
-
-            $fm = $o->featured_media;
-            $media=$this->_getMedia($fm);
-
-            $res[] = [
-                'title' => $title,
-                'excerpt' => $excerpt,
-                'link' => $link,
-                'media' => $media,
-            ];
-        }
         return response()->json([
-                    'data' => ($res),
+                    'data' => $res,
         ]);
     }
 
@@ -56,44 +45,32 @@ class BlogController extends BaseController {
         $media_query = Unirest\Request::get($fm_uri)->body;
         return $media_query->guid->rendered;
     }
-    
+
     /**
      * Returns blogs for today in history
      * @return JSON
      */
-    public function blogsToday(){
+    public function blogsToday() {
         $today = new \DateTime();
         $m = $today->format('m');
         $d = $today->format('d');
         $y = $today->format('Y');
-        
-        $sql = "select p.ID from wp_posts p where p.post_status='publish' and p.post_type='post' and month(p.post_date) = ? and day(p.post_date)=? and year(p.post_date)<>?";
-        $res1=DB::connection('blog')->select($sql, [$m, $d, $y]);
-        
-        $res=[];
-        
-        foreach ($res1 as &$blog)
-        {
-            $id=$blog->ID;
-            $uri="https://blog.rsywx.net/wp-json/wp/v2/posts/$id";
-            
-            $blog= Unirest\Request::get($uri)->body;
-            
-            $viewSql="select meta_value pv from wp_postmeta where post_id=? and meta_key='post_view'";
-            $res2=DB::connection('blog')->select($viewSql, [$id]);
-            
-            $vc=$res2[0]->pv;
-            $title=$blog->title->rendered;
-            $link=$blog->link;
-            $year=$blog->date;
-            
-            $res[]=[
-                'title'=>$title,
-                'link'=>$link,
-                'vc'=>$vc,
-                'year'=> substr($year,0,4),
-            ];
-        }
-        return $res;
+
+        $sql = "
+SELECT wpp.post_title title, wpm.meta_value pv, wpp.id link, year(wpp.post_date) year from wp_posts wpp, wp_postmeta wpm
+where wpp.ID=wpm.post_id
+and month(wpp.post_date)=?
+and day(wpp.post_date)=?
+and year(wpp.post_date)<>?
+and wpm.meta_key='post_view'
+and wpp.post_type='post'
+and wpp.post_status='publish'
+order by wpp.id
+";
+        $res = DB::connection('blog')->select($sql, [$m, $d, $y]);
+        return response()->json([
+                    'data' => $res,
+        ]);
     }
+
 }
